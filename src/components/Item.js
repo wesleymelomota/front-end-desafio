727e89582b7622f98133529c6445545c5f80b5b3
Wesley Melo mota <wesleymota382@gmail.com>
2024-09-14 21:52:06 -0300

    add icon favoritar tarefa

--- src/components/Item.js
+++ src/components/Item.js
@@ -12,6 +12,11 @@ const Item = ({ item, onToggle, onDelete }) => {
       </button></Link>
       
       <button type="button" class="btn btn-primary m-1" onClick={() => onDelete(item.id)}>Excluir</button>
+      <button type="button" class="btn btn-danger m-1" >
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+        </svg>
+      </button>
     </div>
   );
 };
